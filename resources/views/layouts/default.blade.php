ac52e64147efcc53d71cb1f451daf6914a392e28
FaberCastell <vorason.si@gmail.com>
2017-11-08 21:40:58 +0700

    correct

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,8 +4,19 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel 5.5 CRUD example</title>
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <title>Dorm-o-ganize</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+  <link href="css/sb-admin-2.css" rel="stylesheet">
 </head>
 <body>
  
